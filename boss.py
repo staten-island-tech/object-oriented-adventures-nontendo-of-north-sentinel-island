53e57de21c1942893f11f36dd6626f070dc9a964
zijunma <158203969+zijunma@users.noreply.github.com>
2024-04-08 12:07:41 -0400

    main

---
+++ boss.py
@@ -0,0 +1,16 @@
+# Upgrade 1: Health
+def BossDrop1 ():
+    if player == "H":
+        permahp + 20
+    else: 
+        pass
+def BossDrop2 ():
+    if player == "D":
+        permadmg + 5
+    else:
+        pass
+def BossDrop3 ():
+    if player == "G":
+        gold + 50"
+    else: 
+        pass
