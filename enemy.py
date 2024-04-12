12e7c28e034bb573d2890c3f129310e736ab8100
JackDAmico92 <jackd76@nycstudents.net>
2024-04-12 12:02:28 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -1,2 +1,25 @@
 class Enemy:
+    import random
+    def encounter(random):
+     while hp != 0:
+        print('You have encountered a goblin!')
+        enemy = 100
+        move = input("What will you do (attack, heal, run):")
+        if move == "attack":
+            randomnumber = random.randint(1,3)
+            attacknumber = input("pick a number 1-3 :")
+            if attacknumber == randomnumber:
+                enemy - 15
+            else:
+                print("You missed!")
+        if move == "run":
+            break
+        if move == "heal":
+           
+import random             
+Enemy.encounter(random)
+
+        
+
+
     
