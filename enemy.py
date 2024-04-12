c718d4bb3e66f9a493c6304168aeab6197dded89
JackDAmico92 <jackd76@nycstudents.net>
2024-04-12 12:09:57 -0400

    Update enemy.py

--- enemy.py
+++ enemy.py
@@ -15,7 +15,18 @@ class Enemy:
         if move == "run":
             break
         if move == "heal":
-           
+           print(inventory)
+           if potioncount != 0 or bigpotioncount != 0:
+              askheal = input("which potion do you want to use(big potion or potion):")
+              if askheal == "potion":
+                 hp + 25
+                 potioncount - 1
+              if askheal == "bigpotion":
+                 hp + 100
+                 bigpotioncount - 1
+           else:
+              print("You do not have any potions!")
+              
 import random             
 Enemy.encounter(random)
 
