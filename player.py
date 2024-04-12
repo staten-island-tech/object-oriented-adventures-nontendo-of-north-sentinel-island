f46719b3f0574e4c843577539ebc83cc67d2ab74
SidhGupta <sidhg@nycstudents.net>
2024-04-12 12:27:45 -0400

    Changes

--- player.py
+++ player.py
@@ -1,17 +1,16 @@
+import readme.md
+
 class player: 
     def intro():
-     information = ('messi')
+     information = ('Welcome to Object Orientated Aventures! In this game, you will progressivley fight abominations in order to survive.   Each abomination has different stats, characteristics, and may drop useful items. Your goal is to defeat the three dungeons and then the final boss in order to escape. (Dont forget to upgrade our stts in the shop!) Here is some infornmation that can help you on your journey:')
      playername = input('What is the name of your Adventurer?:')
      print(f"Welcome! {playername} to the world of Final Fantasy 178²")
      infoquestion = input('Would you like to read the manual? (Y/N) :')
      if infoquestion == 'Y':
-        print(information)
+        print(information) and print(readme.md)
      start = input('Type START to begin:')
      if start == 'start' or 'START':
-        print('placeholder')
-     else:
-        exit
-
+      print("gl")
 player.intro()
 
 
