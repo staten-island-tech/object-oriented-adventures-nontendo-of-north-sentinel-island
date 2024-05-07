6727dc757d8da2fbbee5991c3ce97e0c325f9a7c
JackDAmico92 <jackd76@nycstudents.net>
2024-05-07 12:24:20 -0400

    awd

--- testing2.py
+++ testing2.py
@@ -6,3 +6,10 @@ team = json.load(f)
 print(team['player'])
 print(team['player']['health'])
 print(team['player']['dmg'])
+print('You have''health left')
+
+
+
+with open('mydata.json', 'w') as f:
+    json.dump(team, f)
+
