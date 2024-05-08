bce06c29540024f443790804aefe946d09b049fa
zijunma <158203969+zijunma@users.noreply.github.com>
2024-05-08 12:09:45 -0400

    main

--- boss.py
+++ boss.py
@@ -3,20 +3,27 @@ import sys
 
 
 
+
 class Dungeon():
-
-
     def print_slow(str):
         for letter in str:
             sys.stdout.write(letter)
             sys.stdout.flush()
             time.sleep(0.04)
     print_slow("You are about to Enter the Slime Dungeon, Enter (Y/N) if you want to Enter or Return Back to the Village")
-    chooseslime = (input(" (Y/N): "))
-    if chooseslime.upper() == "Y":
+    slimechooser = (input(" (Y/N): "))
+    if slimechooser.upper() == "Y":
+        print("")
         print_slow("You have Entered the Dungeon")
         print("")
         print_slow("You Have Encountered A Horde Of Slimes") 
 
-    elif chooseslime.upper() == "N":
-        print("You have returned back to the village")
+    elif slimechooser.upper() == "N":
+        print("")
+        print_slow("You Have Returned to the Village")
+        print(" ")
+        print_slow("Would you like to Stay in the Village or enter the Wilds? ")
+        w = input(" (Y/N): ")
+    
+    if w.upper() == "Y":
+        print("You have entered the Wilds")
