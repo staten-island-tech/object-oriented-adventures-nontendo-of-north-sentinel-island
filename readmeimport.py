3595cd214073cd82e9d9ed57a4884528a2a2b8e1
SidhGupta <sidhg@nycstudents.net>
2024-05-09 11:57:08 -0400

    changes

--- readmeimport.py
+++ readmeimport.py
@@ -1,10 +1,14 @@
 stuff = '''
 # Game Title - Final Fantasy 178² x 0.45
-## Player Character
+# Player Character
 Character has inventory with potions and buff coins
-## Enemies
+# Enemies
 3 main types of enemies (all drop different buff coins)
-### Slimes
+## Example of Attack Code
+```
+
+
+## Slimes
 Green Slimes - 10 hp   
 drops 8 to 12 gold  
 damage per hit: 10  
@@ -15,27 +19,27 @@ damage per hit: 14
 ---- all slimes have 5% chance of slimy token which gives a 10% buff in attack
 
 
-### Spider
+## Spider
 all spiders come in swarms of 5
 Spider - 5 hp, drops 3 gold  
 damage per hit: 9
 
-### Daddy long legs 
+## Daddy long legs 
 10 hp   
 drops 5 gold  
 damage per hit: 15
 
-### Mommy longer legs 
+## Mommy longer legs 
 25 hp     
 drops 15 gold
 damage per hit: 20
 
-### Goblin
+## Goblin
 all goblins come in swarms of 5     
 7 hp    
 drops 7 -9 gold
 
-### Spear goblins
+## Spear goblins
 6 hp    
 drops 7 gold and 1 spears
 
@@ -44,26 +48,50 @@ Obtained from killing spear goblins
 Cannot be reused
 Damage: same damage as one sword attack except there is a 100% hit rate instead of a chance. 
 
-## Dungeons
-### Slime Dungeon 
+
+# Dungeons
+## Slime Dungeon 
 fight 3 blue slimes, then the boss   
 Boss: "Slime King - Amalgamaion of Slime"
-hp: 75   
-Damage per hit: 
+75 HP
+Either 12 or 15 DMG per attack
 
 
+## Goblin Dungeon
+fight two swarms of goblins , or 5 each back to back, then boss
+Boss name: “Gobzilla - Humanity’s Punishment”
+100 HP
+Either 15 or 20 DMG per attack
 
-## Bosses
-### "Slime King - Amalgamaion of Slime"
+## Spider Dungeon
+
+fight a swarm of spiders, then a daddy and mommy long legs
+Boss Name: “Spidorah - The 24 legged Dragon”
+125 HP
+Either 20 or 25 DMG per attack
+
+
+# Bosses
+## "Slime King - Amalgamation of Slime"
+![](https://static.wikia.nocookie.net/terraria_gamepedia/images/9/93/King_Slime.gif/revision/latest/scale-to-width-down/166?cb=20200523113558)
+## "Gobzilla - Humanity's Punishment"
+![](https://www.spriters-resource.com/resources/sheet_icons/51/54580.png?updated=1460951294)
+## "Spidorah - 24 Legged Dragon"
+![](https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1d8e44cc-5e95-40bb-b98a-e5b53d55358d/d5buyir-ce2c320c-e487-4f42-b076-c4c9a0405613.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzFkOGU0NGNjLTVlOTUtNDBiYi1iOThhLWU1YjUzZDU1MzU4ZFwvZDVidXlpci1jZTJjMzIwYy1lNDg3LTRmNDItYjA3Ni1jNGM5YTA0MDU2MTMucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.lIOXlcsPfBhui9jBrPRuCeHH-8lGIcnj-zrTAxAp560)
 
 
 ## Upgrades
 - Values of upgrades (costs) increases in increments of 25
 - NO ARMOR
-- After certain achiecments, The player willincrease in max HP by increments of 25.
+- After certain achievements, The player will increase in max HP by increments of 25.
   
 ## Shop
-Minor otion -  Heals 25% of max hp, cost: 15 gold     
+Minor Potion -  Heals 25% of max hp, cost: 15 gold     
 Major Potion -  Heals 100% of max hp, cost: 60 gold
-"Slime King - Amalgamaion of Slim
+Upgrades to attack: - 
+25 gold - Tier 1 - +15% dmg
+50 gold - Tier 2 - +15% dmg
+75 gold - Tier 3 - +15% dmg
+100 gold - Tier 4 - +15% dmg
+
 '''
