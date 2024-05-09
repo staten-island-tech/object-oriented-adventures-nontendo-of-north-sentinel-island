080c0b79e54e8455f8f6adb6f6431bdc1ca4f63a
JackDAmico92 <jackd76@nycstudents.net>
2024-05-09 12:17:13 -0400

    chanegs

--- village.py
+++ village.py
@@ -2,21 +2,30 @@ import time
 import sys
 
 class village:
-    def enteringvillage():
-        print("You are now entering the village...")
+    def enteringvillage(): 
+      print("You are now entering the village...")
+      while True: 
         villagequestion = input("What would you like to do? (Shop, Explore, NPC Dialouge(WIP))")
-        if villagequestion == "Shop":
+        while villagequestion == "Shop":
             shopquestion = input("What would you like to buy? Here are your options (Upgrade Damage - 50)(Potion - 10)(Big Potion - 50)")
             if shopquestion  == 'Upgrade Damage':
                 print("change this once we get player json thingy working")
+                greatquestion = input("Would you like to keep shopping?(Y/N): ")
+                if greatquestion == "N":
+                    break
             elif shopquestion == "Potion":
                 print("playergold - 10 and potioncount +1 once we get inv and player json working")
+                greatquestion = input("Would you like to keep shopping?(Y/N): ")
+                if greatquestion == "N":
+                    break
             elif shopquestion == 'Big Potion':
                 print("playergold - 50 and bigpotioncount +1 once we get inv and player json working")
-        elif villagequestion == "Explore":
+                greatquestion = input("Would you like to keep shopping?(Y/N): ")
+                if greatquestion == "N":
+                    break
+        if villagequestion == "Explore":
             print("call in exploration class once finished")
         elif villagequestion == "NPC Dialouge":
             print("call in randomized npc dialouge class once finished")
-        else:
-            print("Please type again.")
         
+village.enteringvillage()
