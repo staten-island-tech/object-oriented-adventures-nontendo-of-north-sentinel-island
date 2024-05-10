6cf458680f0edf78e0f2286646ba292f269e30e8
zijunma <158203969+zijunma@users.noreply.github.com>
2024-05-10 12:07:29 -0400

    Update combathandler.py

--- combathandler.py
+++ combathandler.py
@@ -1,27 +1,21 @@
 
-import time
-import sys
 import random 
 
 
 class playercombat:
-        def print_slow(str):
-            for letter in str:
-             sys.stdout.write(letter)
-             sys.stdout.flush()
-             time.sleep(0.001)
-        print_slow("Would you like to Initiate Your Actions? ")
-        initiater = input(" (H/C/E): ")
-        hitpool = random.randint(1,3)
 
-        if initiater.upper() == "C":
-            combatter = print("Choose Between 1-3, to hit your attack!")
+    
+
+        hitpool = random.randint(1,3)
+        while True: 
+            initiater = input("Choose between (H/C/E) to initiate your actions: ")
+            if initiater.upper() == "C":
+                combatter = print("Choose Between 1-3, to hit your attack!")
             guesser = int(input("(1,2,3): "))
-        elif initiater.upper() != "C" or "H" or "E": 
-            print("uh can you type correctly...")
-        if guesser == hitpool:
-            print_slow("You hit your attack!")
-            print("placeholder")
-        elif guesser != hitpool:
-            print_slow("You didn't hit your attack :( ")
             
+            if guesser == hitpool:
+             print("You hit your attack!")
+             print("uhhh we'll get this working once we finish the thing to update enemy data")
+             break
+            if guesser != hitpool:
+             print("You didn't hit your attack :( ")
