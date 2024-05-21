5f7210757467ea44db8c2fa04816aef30d673c6e
JackDAmico92 <jackd76@nycstudents.net>
2024-05-21 12:16:42 -0400

    hjhjj

--- village.py
+++ village.py
@@ -6,7 +6,7 @@ class village:
       from exploration import explore
       print("You are now entering the village...")
       while True: 
-        villagequestion = input("What would you like to do? (Check manual, Shop, Explore, NPC Dialouge(WIP)): ")
+        villagequestion = input("What would you like to do? (Check manual, Shop, Explore, Dungeons): ")
         while villagequestion == "Shop":
             shopquestion = input("What would you like to buy? Here are your options (Upgrade Damage - 50)(Potion - 10)(Big Potion - 50): ")
             if shopquestion  == 'Upgrade Damage':
@@ -26,8 +26,29 @@ class village:
                     break
         if villagequestion == "Explore":
             explore.explorationloop(village.enteringvillage)
-        elif villagequestion == "NPC Dialouge":
-            print("call in randomized npc dialouge class once finished")
-        
-        if villagequestion == "Check manual":
+        elif villagequestion == "Dungeons":
+            dungeonquestion123 = input('Which dungeon will you venture into, The Slime Dungeon, The Goblin Dungeon, or the Spider Dungeon? (1,2,3): ')
+            if dungeonquestion123 == "1":
+                areyousure = input("Are you sure you wish to take on the dungeon? It is highly reccomended to stock up on potions and upgrades beforehand (Y/N) :")
+                if areyousure == "Y":
+                    from dungeon1handler import main
+                    main
+                elif areyousure == "N":
+                    break
+            if dungeonquestion123 == "2":
+                areyousure = input("Are you sure you wish to take on the dungeon? It is highly reccomended to stock up on potions and upgrades beforehand (Y/N) :")
+                if areyousure == "Y":
+                    from dungeon2handler import main
+                    main
+                elif areyousure == "N":
+                    break
+            if dungeonquestion123 == "3":
+                areyousure = input("Are you sure you wish to take on the dungeon? It is highly reccomended to stock up on potions and upgrades beforehand (Y/N) :")
+                if areyousure == "Y":
+                    from dungeon3handler import main
+                    main
+                elif areyousure == "N":
+                    break
+                    
+        elif villagequestion == "Check manual":
             print(stuff)
