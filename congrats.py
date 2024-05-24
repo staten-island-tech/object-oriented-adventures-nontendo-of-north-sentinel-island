be085ce507dfe86481f7b8a08cef78711b9e934e
zijunma <158203969+zijunma@users.noreply.github.com>
2024-05-24 12:28:09 -0400

    Revert "Merge branch 'SIdh' of https://github.com/staten-island-tech/object-oriented-adventures-nontendo-of-north-sentinel-island into SIdh"
    This reverts commit 277d7a5dc687a18123a7c4871c3d943224e9266d, reversing
    changes made to ed5c20b87e006ac233756daa02d9f94e5d1e095a.

--- congrats.py
+++
@@ -1,7 +0,0 @@
-class gameend:
-    def finale():
-        print("Maybe the real Final Fantasy 172² x 0.45 was the friends we made along the way...")
-        print("Congrats for overcoming the challenges and slaying the 3 Mythical Beasts...")
-        print("For this, we present you with the most legendary weapon of them all! The outstanding Bartholomew Jamal Warosa Johnquavious XVI!!!")
-        print("insert sword code here whenever we make it with the code...")
-        print("(Your sword now does 172² x 0.45 damage...)")
